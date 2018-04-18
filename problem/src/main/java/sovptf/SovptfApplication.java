4e917174092496ff8affaa234264753e0b533ad8
yzdalhj <yzdalhj@163.com>
2018-04-18 14:30:29 +0800

    添加

---
+++ problem/src/main/java/sovptf/SovptfApplication.java
@@ -0,0 +1,12 @@
+package sovptf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SovptfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SovptfApplication.class, args);
+	}
+}
---
+++ problem/src/test/java/sovptf/SovptfApplicationTests.java
@@ -0,0 +1,16 @@
+package sovptf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SovptfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
